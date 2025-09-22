0be916906f103215d628086e87a4a16e043ab653
Doan Truong <doantruong120699@gmail.com>
2025-09-22 08:28:22 +0700

    feat: config server for FE

---
+++ frontend/server/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = require("express")();
+const http = require("http").Server(app);
+const path = require("path");
+require("dotenv").config({
+  path: path.resolve(__dirname, ".env"),
+});
+
+const PORT = process.env.PORT || 3001;
+
+app.use(express.static(path.join(__dirname, "../build")));
+
+app.use("/", require("./router"));
+
+app.use((req, res) => {
+  res.status(404).send();
+});
+
+http.listen(PORT, "0.0.0.0", () => {
+  console.log(`AI chat bot app is listening on: http://localhost:${PORT}`);
+});
